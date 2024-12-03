a85f11c2ec8a2cf0ab03e0964eff999bd42a2bd3
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-12-04 00:41:28 +0530

    Update Signup.tsx

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,36 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ArrowLeft, Eye, EyeOff } from 'lucide-react'
+import axios from 'axios';
+import { BACKEND_URL } from '../config';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
+  const [email,setEmail] = useState("");
+  const[password,setPassword] = useState("")
+  const[restaurantName,setRestaurantName] = useState("")
+  const navigate = useNavigate()
+
+  async function handleSignup() {
+    const res = await axios.post(`${BACKEND_URL}/api/v1/restaurant/signup`,{
+      email,
+      password,
+      restaurantName
+    })
+
+    const data = res.data;
+    const userId = data.userId;
+    console.log(data);
+    console.log(userId)
+
+    localStorage.setItem("token",`Bearer ${data.token}`)
+    localStorage.setItem("userId",userId);
+    navigate("/myMenu")
+    
+  }
+
+
 
   return (
     <div className="min-h-screen bg-gray-900 py-0 flex flex-col justify-center sm:px-6 lg:px-8">
@@ -36,6 +63,7 @@ export default function SignUp() {
                   id="restaurant-name"
                   name="restaurant-name"
                   type="text"
+                  onChange={(e)=>{setRestaurantName(e.target.value)}}
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm bg-gray-700 text-white"
                 />
@@ -51,6 +79,7 @@ export default function SignUp() {
                   id="email"
                   name="email"
                   type="email"
+                  onChange={(e)=>{setEmail(e.target.value)}}
                   autoComplete="email"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm bg-gray-700 text-white"
@@ -66,6 +95,7 @@ export default function SignUp() {
                 <input
                   id="password"
                   name="password"
+                  onChange={(e)=>{setPassword(e.target.value)}}
                   type={showPassword ? "text" : "password"}
                   autoComplete="new-password"
                   required
@@ -118,6 +148,7 @@ export default function SignUp() {
             <div>
               <button
                 type="submit"
+                onClick={handleSignup}
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 bg-yellow-400 hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
               >
                 Create Account
