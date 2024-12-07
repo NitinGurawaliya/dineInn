aca7ad75730302a513fdf1ffaa45b56c7d485220
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-12-08 00:02:21 +0530

    Update BottomNav.tsx

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -2,7 +2,7 @@ import { PhoneIcon, BanknoteIcon, MapPinIcon, ShareIcon } from 'lucide-react';
 
 const BottomNavbar = () => {
   return (
-    <div className="fixed bottom-0 left-10 right-10 rounded-full  bg-black flex justify-around items-center py-3 shadow-md z-1000">
+    <div className="fixed bottom-0 left-10 right-10 rounded-full  bg-red-700 flex justify-around items-center py-3 shadow-md z-1000">
       <button className="flex flex-col items-center text-white hover:opacity-80">
         <PhoneIcon size={24} />
         <span className="text-sm mt-1">Call</span>
