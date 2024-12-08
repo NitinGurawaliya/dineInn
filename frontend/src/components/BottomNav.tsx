5e72af472107741311434c7b127bf9dae2879e9a
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-12-08 13:39:53 +0530

    Update BottomNav.tsx

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -2,20 +2,20 @@ import { PhoneIcon, BanknoteIcon, MapPinIcon, ShareIcon } from 'lucide-react';
 
 const BottomNavbar = () => {
   return (
-    <div className="fixed bottom-0 left-10 right-10 rounded-full  bg-red-700 flex justify-around items-center py-3 shadow-md z-1000">
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+    <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full  bg-orange-800 flex justify-around items-center py-3 shadow-md z-1000">
+      <button className="flex flex-col items-center text-black hover:opacity-80">
         <PhoneIcon size={24} />
         <span className="text-sm mt-1">Call</span>
       </button>
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+      <button className="flex flex-col items-center text-black hover:opacity-80">
         <BanknoteIcon size={24} />
         <span className="text-sm mt-1">Payment</span>
       </button>
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+      <button className="flex flex-col items-center text-black hover:opacity-80">
         <MapPinIcon size={24} />
         <span className="text-sm mt-1">Navigate</span>
       </button>
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+      <button className="flex flex-col items-center text-black hover:opacity-80">
         <ShareIcon size={24} />
         <span className="text-sm mt-1">Share</span>
       </button>
