6949971ad52ed321756ab0c35cee60af2e28b775
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-12-09 22:14:32 +0530

    Update index.ts

--- backend/src/zod/index.ts
+++ backend/src/zod/index.ts
@@ -4,7 +4,9 @@ import zod from "zod";
 export const restaurantSignupSchema = zod.object({
     restaurantName: zod.string(),
     email:zod.string().email(),
-    password:zod.string().min(6)
+    password:zod.string().min(6),
+    ContactNum:zod.string(),
+    City:zod.string()
 })
 
 export const restaurantSigninSchema = zod.object({
