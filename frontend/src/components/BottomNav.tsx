77da97838310eaf813100d4542a6df0c00089b37
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-12-09 23:07:58 +0530

    Update BottomNav.tsx

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -9,19 +9,19 @@ const BottomNavbar:React.FC<BottomNavProps> = ({contact}) => {
     <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full  bg-black flex justify-around items-center py-3 shadow-md z-1000">
       <button 
       onClick={() => window.location.href = `tel:${contact}`}
-       className="flex flex-col items-center text-black hover:opacity-80">
+       className="flex flex-col items-center text-white hover:opacity-80">
         <PhoneIcon size={24} />
         <span className="text-sm mt-1">Call</span>
       </button>
-      <button className="flex flex-col items-center text-black hover:opacity-80">
+      <button className="flex flex-col items-center text-white hover:opacity-80">
         <BanknoteIcon size={24} />
         <span className="text-sm mt-1">Payment</span>
       </button>
-      <button className="flex flex-col items-center text-black hover:opacity-80">
+      <button className="flex flex-col items-center text-white hover:opacity-80">
         <MapPinIcon size={24} />
         <span className="text-sm mt-1">Navigate</span>
       </button>
-      <button className="flex flex-col items-center text-black hover:opacity-80">
+      <button className="flex flex-col items-center text-white hover:opacity-80">
         <ShareIcon size={24} />
         <span className="text-sm mt-1">Share</span>
       </button>
