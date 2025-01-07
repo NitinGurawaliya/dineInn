ec6d7fff0869490697f83960396b81092a3ed383
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-08 00:35:50 +0530

    Update BottomNav.tsx

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,19 +1,29 @@
 import { PhoneIcon, BanknoteIcon, MapPinIcon, ShareIcon } from 'lucide-react';
+import { useState } from 'react';
+import PaymentDialog from './PaymentDialog'; // Adjust the path as needed
+import ShareDialog from './ShareDialog';
 
-interface BottomNavProps{
-  contact:string;
+interface BottomNavProps {
+  contact: string;
+  upiQr: string; 
+  link:string
 }
 
-const BottomNavbar:React.FC<BottomNavProps> = ({contact}) => {
+const BottomNavbar: React.FC<BottomNavProps> = ({ contact, upiQr,link }) => {
+  const [isPaymentDialogOpen, setIsPaymentDialogOpen] = useState(false);
+  const[isShareDialogOpen,setIsShareDialogOpen] = useState(false)
+
   return (
-    <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full  bg-black flex justify-around items-center py-3 shadow-md z-1000">
+    <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full bg-black flex justify-around items-center py-3 shadow-md z-10">
       <button 
-      onClick={() => window.location.href = `tel:${contact}`}
-       className="flex flex-col items-center text-white hover:opacity-80">
+        onClick={() => window.location.href = `tel:${contact}`}
+        className="flex flex-col items-center text-white hover:opacity-80">
         <PhoneIcon size={24} />
         <span className="text-sm mt-1">Call</span>
       </button>
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+      <button
+        onClick={() => setIsPaymentDialogOpen(true)} // Open the dialog
+        className="flex flex-col items-center text-white hover:opacity-80">
         <BanknoteIcon size={24} />
         <span className="text-sm mt-1">Payment</span>
       </button>
@@ -22,9 +32,16 @@ const BottomNavbar:React.FC<BottomNavProps> = ({contact}) => {
         <span className="text-sm mt-1">Navigate</span>
       </button>
       <button className="flex flex-col items-center text-white hover:opacity-80">
-        <ShareIcon size={24} />
+        <ShareIcon onClick={()=>{setIsShareDialogOpen(true)}} size={24} />
         <span className="text-sm mt-1">Share</span>
       </button>
+      
+
+      {/* Payment Dialog */}
+      <PaymentDialog open={isPaymentDialogOpen} onClose={() => setIsPaymentDialogOpen(false)} upiQr={upiQr} />
+
+      <ShareDialog link={link} open={isShareDialogOpen}  onClose={()=>{setIsShareDialogOpen(false)}}/>
+
     </div>
   );
 };
