cfbedfaaf16f91d5ec573f05bd12c0942a4aeda8
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-08 00:34:43 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4003;
 
 app.use(express.json());
 app.use(cors())
