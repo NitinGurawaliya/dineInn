29a7fd47b7a4d0e03c9c6c70479b354bd8495e35
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-13 00:16:13 +0530

    Update BottomNav.tsx

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { PhoneIcon, BanknoteIcon, MapPinIcon, ShareIcon } from 'lucide-react';
+import { PhoneIcon, BanknoteIcon, ShareIcon } from 'lucide-react';
 import { useState } from 'react';
 import PaymentDialog from './PaymentDialog'; // Adjust the path as needed
 import ShareDialog from './ShareDialog';
@@ -14,7 +14,7 @@ const BottomNavbar: React.FC<BottomNavProps> = ({ contact, upiQr,link }) => {
   const[isShareDialogOpen,setIsShareDialogOpen] = useState(false)
 
   return (
-    <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full bg-black flex justify-around items-center py-3 shadow-md z-10">
+    <div className="fixed bottom-0 left-10 right-10 md:left-80 md:right-80 rounded-full bg-orange-900 flex justify-around items-center py-3 shadow-md z-10">
       <button 
         onClick={() => window.location.href = `tel:${contact}`}
         className="flex flex-col items-center text-white hover:opacity-80">
@@ -24,13 +24,13 @@ const BottomNavbar: React.FC<BottomNavProps> = ({ contact, upiQr,link }) => {
       <button
         onClick={() => setIsPaymentDialogOpen(true)} // Open the dialog
         className="flex flex-col items-center text-white hover:opacity-80">
-        <BanknoteIcon size={24} />
+        <BanknoteIcon size={33} />
         <span className="text-sm mt-1">Payment</span>
       </button>
-      <button className="flex flex-col items-center text-white hover:opacity-80">
+      {/* <button className="flex flex-col items-center text-white hover:opacity-80">
         <MapPinIcon size={24} />
         <span className="text-sm mt-1">Navigate</span>
-      </button>
+      </button> */}
       <button className="flex flex-col items-center text-white hover:opacity-80">
         <ShareIcon onClick={()=>{setIsShareDialogOpen(true)}} size={24} />
         <span className="text-sm mt-1">Share</span>
