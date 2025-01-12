fd3408aaf05a0bf51405cff2fc18620892bf901e
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-13 00:16:04 +0530

    Update index.ts

--- backend/src/zod/index.ts
+++ backend/src/zod/index.ts
@@ -17,6 +17,9 @@ export const restaurantOnboardingSchema  = zod.object({
     restaurantName: zod.string(),
     contactNum:zod.string(),
     city:zod.string(),
-    WeekdaysWorking:zod.string(),
-    WeekendWorking:zod.string()
+    WeekdaysWorking:zod.string().optional(),
+    WeekendWorking:zod.string().optional(),
+    Instagram:zod.string().optional(),
+    Facebook:zod.string().optional()
+
 })      
