8e03d1cb24b9fad39360ea3698f8bcf5c9b20677
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-13 00:16:16 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,18 +1,48 @@
-
+import { FaFacebook, FaInstagram,FaWhatsappSquare } from "react-icons/fa"
 
 interface FooterProps{
   weekdayHours:string,
-  weekendHours:string
+  weekendHours:string,
+  email:string,
+  contact:string,
+  instagram:string,
+  facebook:string
 }
 
-const Footer:React.FC<FooterProps>= ({weekdayHours,weekendHours}) => {
+const Footer:React.FC<FooterProps>= ({weekdayHours,weekendHours,email,contact,instagram,facebook}) => {
   return (
     <footer className="bg-black border-t-2 border-t-yellow-300 px-4 text-white py-10">
       <div className="container p-4 mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
         {/* About Us */}
-       
 
         <div>
+          <h3 className="text-lg font-bold mb-4">ABOUT US</h3>
+          <button onClick={()=>{
+            const url = `https://www.instagram.com/?${instagram}`
+             window.open(url, '_blank');
+          }} className="p-4">
+            <FaInstagram size={33 } />
+          </button>
+
+          <button onClick={()=>{
+            const url = `https://www.facebook.com/${facebook}`
+             window.open(url, '_blank');
+          }} className="p-4">
+            <FaFacebook size={33} />
+          </button>
+
+          <button onClick={()=>{
+            const url = `https://api.whatsapp.com/send?phone=${contact}`
+             window.open(url,'_blank');
+          }} className="p-4" >
+            <FaWhatsappSquare size={33} />
+          </button>
+
+
+        </div>
+       
+
+        <div className="justify-items-center">
           <h3 className="text-lg font-bold mb-4">WORKING HOURS-</h3>
           <ul className="space-y-2">
             <li className="flex items-center justify-start gap-x-4">
@@ -51,14 +81,15 @@ const Footer:React.FC<FooterProps>= ({weekdayHours,weekendHours}) => {
         <div>
           <h3 className="text-lg font-bold mb-4">CONTACT US</h3>
           <ul className="space-y-2">
-            <li>
+            <button onClick={() => window.location.href = `tel:${contact}`}>
               <span className="inline-block mr-2">📱</span>
-              +919466444175
-            </li>
-            <li>
+              +{contact}
+            </button>
+            <br></br>
+            <button onClick={()=>{window.location.href=`mailto:${email}`}}>
               <span className="inline-block mr-2">📧</span>
-              dinein2003@gmail.com
-            </li>
+              {email}
+            </button>
             <li>
               <span className="inline-block mr-2">📍</span>
               GURUGRAM, IND
@@ -66,13 +97,7 @@ const Footer:React.FC<FooterProps>= ({weekdayHours,weekendHours}) => {
           </ul>
         </div>
 
-        <div>
-          <h3 className="text-lg font-bold mb-4">ABOUT US</h3>
-          <p>
-            We are dedicated to providing the best services to our customers.
-            Your satisfaction is our priority.
-          </p>
-        </div>
+        
 
         {/* Opening Hours */}
         
