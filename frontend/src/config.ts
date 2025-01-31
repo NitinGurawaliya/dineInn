4bce95d9fe4e8e197718b9407fd9d9f33511e924
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-01-31 23:58:44 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://dineinn-aah4.onrender.com/"
+export const BACKEND_URL = "https://dineinn-aah4.onrender.com"
 export const FRONTEND_URL = "https://dine-inn.vercel.app"
 
 // "http://localhost:4004"
