15afcdb1849f1ba21938fc37ea3b57da58ec017e
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-02-08 15:39:19 +0530

    Create Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import HomePage from "../components/HomeComponent";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
+
+interface Data{
+    id: number,
+    restaurantName: string,
+    Instagram: string,
+    location: string,
+    whatsapp: string,
+    contactNum: string,
+    Logo: string}
+
+export default function Home(){
+    const {id} = useParams();
+    const [data,setData] = useState<Data | null>(null)
+
+    async function resDetails() {
+        
+        console.log(id)
+        const res = await axios.get(`${BACKEND_URL}/api/v1/restaurant/${id}`);
+
+        console.log(res.data)
+        setData(res.data.resName)
+        
+    }
+
+    useEffect(()=>{
+        resDetails()
+    },[])
+
+    if (!data) return <div>Loading...</div>; // Handle loading state properly
+
+
+    return <div>
+       <HomePage
+        id={id || ""}
+        restaurantName={data.restaurantName}
+        instagram={data.Instagram}
+        location={data.location}
+        whatsapp={data.whatsapp}
+        contactNumber={data.contactNum}
+        logo={data.Logo}
+      />
+
+    </div>
+}
