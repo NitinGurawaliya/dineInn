30e1a26b4c20407701cb31fed1d1218ced7be502
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2026-04-21 15:32:42 +0530

    Update BACKEND_URL to new Vercel endpoint

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://dineinn-aah4.onrender.com"
+export const BACKEND_URL = "https://dine-inn-34c2.vercel.app"
 export const FRONTEND_URL = "https://dine-inn.vercel.app"
